4776d9b001ee82f5c4e8110cebabe54d98163d1d
Avery Slomnicki <averyslomo@Averys-MacBook-Pro-3.local>
2022-03-15 12:09:17 -0400

    user class stubs

---
+++ User.java
@@ -0,0 +1,10 @@
+public class User {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public User(int age, String firstName, String lastName){
+        
+    }
+
+}
