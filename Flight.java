327ddb34273260d066760e5aaeecd13c43bca1b4
peytontucker <peytonmtucker@gmail.com>
2022-03-16 16:28:34 -0400

    finished Flight class

--- Flight.java
+++ Flight.java
@@ -1,31 +1,142 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Flight {
-    private String flightID;
+    private UUID flightID;
     private String departureCode;
     private String arrivalCode;
     private LocalDate departureDate;
     private LocalTime departureTime;
     private LocalDate arrivalDate;
-    private LocalTime arrivaltime;
-    private int firstSeats;
-    private int businessSeats;
-    private int economySeats;
+    private LocalTime arrivalTime;
+    private int numAvailFirstSeats;
+    private int numAvailBusinessSeats;
+    private int numAvailEconomySeats;
     private boolean isFull;
     private boolean isInternational;
     private ArrayList<Seat> seats;
     
-    public Flight(String departureCode, String arrivalCode, LocalDate departureDate, LocalTime departureTime, LocalDate arrivalDate, LocalTime arrivalTime, int firstSeats, int businessSeats, int economySeats, boolean isFull){
-
+    public Flight(String flightID, String departureCode, String arrivalCode, LocalTime departureTime, LocalTime arrivalTime, int numAvailFirstSeats,
+     int numAvailBusinessSeats, int numAvailEconomySeats, boolean isFull, boolean isInternational) {
+        this.flightID = UUID.fromString(flightID);
+        this.departureCode = departureCode;
+        this.arrivalCode = arrivalCode;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.numAvailFirstSeats = numAvailFirstSeats;
+        this.numAvailBusinessSeats = numAvailBusinessSeats;
+        this.numAvailEconomySeats = numAvailEconomySeats;
+        this.isFull = isFull;
+        this.isInternational = isInternational;
     }
 
-    private void setIsFull(int firstSeats, int businessSeats, int economySeats){
-
+    public boolean getIsFull() {
+        return this.isFull;
     }
 
-    public boolean getIsFull(){
-        return false;
+    public UUID getFlightID() {
+        return this.flightID;
+    }
+
+    public String getDepartureCode() {
+        return this.departureCode;
+    }
+
+    public String getArrivalCode() {
+        return this.arrivalCode;
+    }
+
+    public LocalDate getDepartureDate() {
+        return this.departureDate;
+    }
+
+    public LocalTime getDepartureTime() {
+        return this.departureTime;
+    }
+
+    public LocalDate getArrivalDate() {
+        return this.arrivalDate;
+    }
+
+    public LocalTime getArrivalTime() {
+        return this.arrivalTime;
+    }
+
+    public int getNumAvailFirstSeats() {
+        return this.numAvailFirstSeats;
+    }
+
+    public int getNumBusinessSeats() {
+        return this.numAvailBusinessSeats;
+    }
+
+    public int getNumEconomySeats() {
+        return this.numAvailEconomySeats;
+    }
+
+    public boolean getIsInternational() {
+        return this.isInternational;
+    }
+
+    public ArrayList<Seat> getSeats() {
+        return this.seats;
+    }
+
+    public void setFlightID(UUID flightID) {
+        this.flightID = flightID;
+    }
+
+    public void setDepartureCode(String departureCode) {
+        this.departureCode = departureCode;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setNumAvailFirstSeats(int numAvailFirstSeats) {
+        this.numAvailFirstSeats = numAvailFirstSeats;
+        if (this.numAvailFirstSeats < 1) checkForFull();
+    }
+
+    public void setNumAvailBusinessSeats(int numAvailBusinessSeats) {
+        this.numAvailBusinessSeats = numAvailBusinessSeats;
+        if (this.numAvailBusinessSeats < 1) checkForFull();
+    }
+
+    public void setNumAvailEconomySeats(int numAvailEconomySeats) {
+        this.numAvailEconomySeats = numAvailEconomySeats;
+        if (this.numAvailEconomySeats < 1) checkForFull();
+    }
+
+    public void setIsFull(boolean isFull) {
+        this.isFull = isFull;
+    }
+
+    public void setIsInternational(boolean isInternational) {
+        this.isInternational = isInternational;
+    }
+
+    public void setSeats(ArrayList<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public void checkForFull() {
+        if (this.numAvailFirstSeats < 1 && this.numAvailBusinessSeats < 1 && this.numAvailEconomySeats < 1) this.isFull = true;
+        else this.isFull = false;
     }
 }
