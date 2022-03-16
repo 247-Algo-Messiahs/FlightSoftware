b4b721188e4db4ae52b3492a18689a3495e6706b
peytontucker <peytonmtucker@gmail.com>
2022-03-15 23:10:06 -0400

    fixed preferences

--- Preferences.java
+++ Preferences.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Preferences {
     private SeatType seatType;
     private SeatClass seatClass;
-    private HotelRoom ArrayList<RoomTrait> traits;
+    private ArrayList<RoomTrait> hotelRoom;
     private String airportCode;
 
     public Preferences(SeatType seatType, SeatClass seatClass, ArrayList<RoomTrait> hotelRoom, String airportCode){
