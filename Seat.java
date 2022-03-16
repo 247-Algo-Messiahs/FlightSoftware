2b73788a6885c096d6143ddd07d5db38981de8e5
peytontucker <peytonmtucker@gmail.com>
2022-03-16 16:35:26 -0400

    finished Seat class

--- Seat.java
+++ Seat.java
@@ -1,8 +1,44 @@
 public class Seat {
 
-    private SeatType type;
+    private SeatType seatType;
     private SeatClass seatClass;
-    private String number;
+    private String seatNumber;
     private String flightID;
-    private boolean booked;
+    private boolean isBooked;
+
+    public SeatType getSeatType() {
+        return this.seatType;
+    }
+
+    public SeatClass getSeatClass() {
+        return this.seatClass;
+    }
+
+    public String getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public String getFlightID() {
+        return this.flightID;
+    }
+
+    public boolean getIsBooked() {
+        return this.isBooked;
+    }
+
+    public void setSeatType(SeatType seatType) {
+        this.seatType = seatType;
+    }
+
+    public void setSeatClass(SeatClass seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
 }
