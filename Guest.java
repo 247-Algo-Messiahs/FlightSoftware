9a94fd9ebe6b5c0f5e79e0013324b79214cd2bfc
Avery Slomnicki <averyslomo@Averys-MacBook-Pro-3.local>
2022-03-17 12:36:33 -0400

    Guest getters/setters

--- Guest.java
+++ Guest.java
@@ -1,18 +1,48 @@
+import java.util.UUID;
+
 public class Guest {
     private String name;
     private int age;
-    private String seatID;
+    private UUID seatID;
+    private boolean needSeat;
 
     public Guest(int age, String firstName, String lastName){
 
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public UUID getSeatID(){
+        return this.seatID;
+    }
+
     public boolean getNeedSeat(){
-        return true;
+        return this.needSeat;
     }
 
-    private void setNeedSeat(int age){
 
+    public void setName(String name){
+        this.name = name;
     }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setSeatID(UUID seatID){
+        this.seatID = seatID;
+    }
+
+    public void setNeedSeat(boolean needSeat){
+        this.needSeat = needSeat;
+    }
+
+ 
 
 }
