3b81d2231a759e55b6c2496bf4c6d667cc6274ef
Avery Slomnicki <averyslomo@Averys-MacBook-Pro-3.local>
2022-03-17 12:49:45 -0400

    hotel class getters/setters

--- Hotel.java
+++ Hotel.java
@@ -1,22 +1,65 @@
-import java.util.Arrays;
+import java.util.UUID;
+
 public class Hotel {
-    private int hotelID;
+    private UUID hotelID;
     private int hotelRating;
     private int capacity;
-    private String name;
+    private String hotelName;
     private String location;
-    private HotelRoom hotelRoomArray[];
+    private HotelRoom[] rooms;
 
     public Hotel(){
 
     }
+    public UUID getHotelID(){
+        return this.hotelID;
+    }
+
+    public int getHotelRating(){
+        return this.hotelRating;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public String getHotelName(){
+        return this.hotelName;
+    }
 
     public String getLocation(){
-        return null;
+        return this.location;
     }
 
     public HotelRoom[] getRooms(){
-        return null;
+        return this.rooms;
     }
+
+
+    public void setHotelID(UUID hotelID){
+        this.hotelID = hotelID;
+    }
+
+    public void setHotelRating(int hotelRating){
+        this.hotelRating = hotelRating;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public void setHotelName(String hotelName){
+        this.hotelName = hotelName;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void getRooms(HotelRoom[] rooms){
+        this.rooms = rooms;
+    }
+
+  
 
 }
