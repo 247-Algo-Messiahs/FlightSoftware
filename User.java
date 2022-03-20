af17073ef32c490f07d570790af0f1e524d8dc78
peytontucker <peytonmtucker@gmail.com>
2022-03-20 03:29:13 -0400

    finished facade stub and began ui

--- User.java
+++ User.java
@@ -3,8 +3,16 @@ public class User {
     private String firstName;
     private String lastName;
 
+    public User() {
+        this.firstName = "NONE_GIVEN";
+        this.lastName = "NONE_GIVEN";
+        this.age = 0;
+    }
+
     public User(int age, String firstName, String lastName){
-        
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
 }
