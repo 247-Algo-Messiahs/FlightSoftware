9853e4428a5b0d3cfd09472b057f88a5d9d8271d
Avery Slomnicki <averyslomo@Averys-MacBook-Pro-3.local>
2022-03-24 12:56:51 -0400

    UserInterface update

--- UserInterface.java
+++ UserInterface.java
@@ -5,6 +5,8 @@ public class UserInterface {
     private static UserInterface userInterface;
     private Scanner scanner;
     private Facade facade;
+    private static final String WELCOME_MESSAGE = "Welcome to Canoe’s Flight Booking Software!";
+    private String[] mainMenuOptions = {"Log In","Create an Account","Continue as Guest","Search for Flight","Exit"};
     
     private UserInterface() {
         this.scanner = new Scanner(System.in);
@@ -33,8 +35,59 @@ public class UserInterface {
 
         System.out.println(testHotel);
 
-        System.out.println(testHotel.getRooms().get(0));
+        System.out.println(testHotel.getRooms().get(0)); 
+
+
+        //TERMINAL STARTS HERE
+        System.out.println(WELCOME_MESSAGE);
+
+        while(true){
+            displayMainMenu();
+
+            int userCommand = getUserSelection(mainMenuOptions.length);
+			
+			if(userCommand == -1) {
+				System.out.println("Not a valid command");
+				continue;
+            }
+
+            if(userCommand == mainMenuOptions.length -1) break;
+
+            switch(userCommand) {
+                case(0):
+                        logIn();
+                        break;
+                case(1):
+                        createAccount();
+                        break;
+                case(2):
+                        continueAsGuest();
+                        break;
+                case(3):
+                        searchForFlight();
+                        break;
+                case(4):
+                        logOut();
+                        break;
+            }
+        }
+
+        System.out.println("Thank you for choosing Canoe Booking! Happy Travels!");
+
     }   
+    public void printHeading(String heading) {
+        System.out.println("**********" + heading + "**********");
+    }
+
+    private void displayMainMenu(){
+        printHeading(" Main Menu ");
+        for(int i=0;i<mainMenuOptions.length;i++){
+            System.out.println((i+1) + ". " + mainMenuOptions[i]);
+        }
+    System.out.println("\n");
+    scanner.nextInt();
+    scanner.nextLine();
+    }
 
     private int getUserSelection(int numCommands) {
         String input = scanner.nextLine();
@@ -45,9 +98,29 @@ public class UserInterface {
 
     }
 
-    public void printHeading(String heading) {
-        System.out.println("**********" + heading + "**********");
+    private void logIn(){
+        printHeading(" Log In ");
+        System.out.println("Please enter your login information:");
+        System.out.println("Username:");
+        String username = scanner.nextLine();
+        System.out.println("Password:");
+        String password = scanner.nextLine();
+        facade.logIn(username, password);
     }
+    private void createAccount(){
+
+    }
+    private void continueAsGuest(){
+
+    }
+    private void searchForFlight(){
+
+    }
+    private void logOut(){
+
+    }
+
+   
     public static void main(String[] args) {
         UserInterface UI = new UserInterface();
         UI.run();
