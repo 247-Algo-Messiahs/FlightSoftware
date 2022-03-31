63d9cc4c1dc99af5963a2e7ca0a122bb5f71a282
peytontucker <peytonmtucker@gmail.com>
2022-03-30 23:09:24 -0400

    javadoc'd Guest and BedType

--- BedType.java
+++ BedType.java
@@ -1,3 +1,7 @@
+/**
+ * @author Avery Slomnicki, Austin Hanson
+ * An enum for the available types of beds in a hotel room.
+ */
 public enum BedType {
     DOUBLE,
     QUEEN,
--- Guest.java
+++ Guest.java
@@ -1,9 +1,19 @@
-import java.util.UUID;
+/**
+ * @author Avery Slomnicki, Austin Hanson, Peyton Tucker
+ * A class to keep track of guests who are accompanying user objects of the RegisteredUser class on flights
+ */
 
 public class Guest extends User{
     private int seatID;
     private boolean needSeat;
 
+    /**
+     * Constructs a new instance of the Guest class
+     * @param age the age of the guest
+     * @param firstName the first name of the guest
+     * @param lastName the last name of the guest
+     * @param seatID the seatID of the guest's seat on the flight they are taking
+     */
     public Guest(int age, String firstName, String lastName, int seatID){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -11,29 +21,38 @@ public class Guest extends User{
         this.seatID = seatID;
     }
 
+    /**
+     * Returns the name of this Guest object
+     * @return the name of this Guest object
+     */
     public String getName(){
         return this.firstName + " " + this.lastName;
     }
 
+    /**
+     * returns the age of the guest this Object represents
+     * @return this guest object's age value
+     */
     public int getAge(){
         return this.age;
     }
 
+    /**
+     * Gets the seatID of this guest
+     * @return this object's seatID
+     */
     public int getSeatID(){
         return this.seatID;
     }
 
-    public boolean getNeedSeat(){
-        return this.needSeat;
-    }
-
+    /**
+     * Sets the seatID of this Guest object
+     * @param seatID the seatID to set
+     */
     public void setSeatID(int seatID){
         this.seatID = seatID;
     }
 
-    public void setNeedSeat(boolean needSeat){
-        this.needSeat = needSeat;
-    }
 
  
 
