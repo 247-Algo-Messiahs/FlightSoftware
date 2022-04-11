d2e3921fd8e9b2a369f8bbbcdf040e67707feecc
Avery Slomnicki <averyslomo@Averys-MBP-3.lan>
2022-04-11 19:35:21 -0400

    Added a test to FlightFilterTest

--- FlightFilterTest.java
+++ FlightFilterTest.java
@@ -39,7 +39,7 @@ public void tearDown(){
 
 }
 @Test 
-void filterDuration(){
+void testDurationFilter(){
     FlightFilter f = new FlightFilter();
 
     UUID flightID = UUID.randomUUID();
@@ -49,24 +49,56 @@ void filterDuration(){
         Seat seat = new Seat(seatID, "AISLE", "BUSINESS", "1A", flightID, false);
         oneSeat.add(seat);
 
-    ArrayList<Flight> randomFlightFilter = new ArrayList<Flight>();
-        Flight flight1 = new Flight(flightID, "ATL", "HND", LocalTime.parse("07:00"), LocalTime.parse("09:00"), 3, 3, 3, false, false, oneSeat, 100, null);
-        randomFlightFilter.add(flight1);
-        Flight flight2 = new Flight(flightID, "ATL", "HND", LocalTime.parse("07:00"), LocalTime.parse("10:00"), 3, 3, 3, false, false, oneSeat, 100, null);
-        randomFlightFilter.add(flight2);
-        Flight flight3 = new Flight(flightID, "ATL", "HND", LocalTime.parse("07:00"), LocalTime.parse("11:00"), 3, 3, 3, false, false, oneSeat, 100, null);
-        randomFlightFilter.add(flight3);
-        Flight flight4 = new Flight(flightID, "ATL", "HND", LocalTime.parse("07:00"), LocalTime.parse("12:00"), 3, 3, 3, false, false, oneSeat, 100, null);
-        randomFlightFilter.add(flight4);
+    ArrayList<Flight> randomDurationFilter = new ArrayList<Flight>();
+        Flight flight1 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("07:00"), LocalTime.parse("09:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomDurationFilter.add(flight1);
+        Flight flight2 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("07:00"), LocalTime.parse("10:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomDurationFilter.add(flight2);
+        Flight flight3 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("07:00"), LocalTime.parse("11:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomDurationFilter.add(flight3);
+        Flight flight4 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("07:00"), LocalTime.parse("12:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomDurationFilter.add(flight4);
        
-        flightList.setFlights(randomFlightFilter);
+        flightList.setFlights(randomDurationFilter);
 
-    ArrayList<Flight> randomFlightFilterTest = new ArrayList<Flight>();
-        randomFlightFilterTest.add(flight4);
-        randomFlightFilterTest.add(flight2);
-        randomFlightFilterTest.add(flight3);
-        randomFlightFilterTest.add(flight1);
+    ArrayList<Flight> randomDurationFilterTest = new ArrayList<Flight>();
+    randomDurationFilterTest.add(flight4);
+    randomDurationFilterTest.add(flight2);
+    randomDurationFilterTest.add(flight3);
+    randomDurationFilterTest.add(flight1);
 
-        assertEquals(randomFlightFilter,f.filterFlights(FlightTrait.DURATION, randomFlightFilterTest));
+        assertEquals(randomDurationFilter,f.filterFlights(FlightTrait.DURATION, randomDurationFilterTest));
+    }
+@Test
+void testTakeoffFilter(){
+    FlightFilter f = new FlightFilter();
+
+    UUID flightID = UUID.randomUUID();
+
+    ArrayList<Seat> oneSeat = new ArrayList<Seat>();
+        int seatID = 3;
+        Seat seat = new Seat(seatID, "AISLE", "BUSINESS", "1A", flightID, false);
+        oneSeat.add(seat);
+
+    ArrayList<Flight> randomTakeoffFilter = new ArrayList<Flight>();
+        Flight flight1 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("04:00"), LocalTime.parse("05:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomTakeoffFilter.add(flight1);
+        Flight flight2 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("06:00"), LocalTime.parse("07:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomTakeoffFilter.add(flight2);
+        Flight flight3 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("08:00"), LocalTime.parse("09:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomTakeoffFilter.add(flight3);
+        Flight flight4 = new Flight(flightID, "DFW", "JFK", LocalTime.parse("10:00"), LocalTime.parse("12:00"), 3, 3, 3, false, false, oneSeat, 100, null);
+        randomTakeoffFilter.add(flight4);
+
+        flightList.setFlights(randomTakeoffFilter);
+
+        ArrayList<Flight> randomTakeoffFilterTest = new ArrayList<Flight>();
+        randomTakeoffFilterTest.add(flight3);
+        randomTakeoffFilterTest.add(flight1);
+        randomTakeoffFilterTest.add(flight4);
+        randomTakeoffFilterTest.add(flight2);
+
+        assertEquals(randomTakeoffFilter,f.filterFlights(FlightTrait.TAKEOFF_TIME, randomTakeoffFilterTest));
+
 }
 }
